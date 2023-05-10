385f1fd7b76558857ed7951087f74ede2aa9d8bd
ImoriSama <80395754+ImoriSama@users.noreply.github.com>
2023-05-10 19:51:26 +0200

    styles

--- index.php
+++ index.php
@@ -27,11 +27,12 @@
         <div class="container-fluid">
           <div class="row">           
             <div class="col-md-9 col-sm-9"> 
-              <div class="text-center">
-              <h1>Bienvenue sur Akatuski</h1> 
-              <h2>le site reference pour en apprendre plus sur l'Akatsuki</h2> 
-
-              <h3>C'est quoi? C'est qui?</h3>
+              <div class="text-center marg-both-50">
+              <h1>Bienvenue sur <span class="akatsuki">Akatuski</span></h1>
+              <div class="marg-bt-50"></div>
+              <h2><u>Le site référence</u> pour en apprendre plus sur l'Akatsuki</h2>
+              <h4><i>C'est quoi? C'est qui?</i></h4>
+              <div class="marg-bt-50"></div>
               <p>L'Akatsuki (暁, Akatsuki, signifiant littéralement : « Aube » ou « Lever du jour ») est un groupe de shinobi, affranchi du système dépendant des villages cachés, représentant l'organisation criminelle la plus activement recherchée dans le monde ninja. Au cours de plusieurs décennies, Akatsuki a connu différents dirigeants et buts. Bien que chaque recrue ait un passé criminel, tous cherchent à rendre le monde meilleur par leurs propres moyens. Exceptés Konan et Nagato, ses membres sont tous des ninjas répertoriés au Bingo Book. Son objectif affiché est la domination du monde et pour ce faire, leurs membres jouent les mercenaires auprès des différents pays et tentent de capturer tous les bijû afin de ramener Jûbi à la vie et d'instaurer une « paix » durable.</p>
 
             </div>

4be4034a565afae19c9ea26897abd212d3e6f6a2
ImoriSama <80395754+ImoriSama@users.noreply.github.com>
2023-05-11 15:01:39 +0200

    Update pageadmin.php

--- script/pageadmin.php
+++ script/pageadmin.php
@@ -13,22 +13,41 @@ $password = " ";
   <link rel="stylesheet" href="../css/bootstrap.min.css">
   <link rel="stylesheet" href="../css/style.css">
 </head>
-<body class="back">
+<body>
 <center>
-<form class="marg-150" method="post" action="conexadmin.php">
-    <table>
-        <tr>
-            <td><p>Login</p></td>
-            <td><input type="text" required name="username"></td>
-        </tr>
-        <tr>
-            <td><p>Code access</p></td>
-            <td><input type="password" required name="pass"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="envoyer"></td>
-            <td><input type="reset" name="Annuler"></td>
-        </tr>
+<form method="post" action="conexadmin.php">
+<section class="vh-100 gradient-custom">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="card bg-dark text-white" style="border-radius: 1rem;">
+          <div class="card-body p-5 text-center">
+
+            <div class="mb-md-5 mt-md-4 pb-5">
+
+              <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+              <p class="text-white-50 mb-5">Entrez votre username et mot de passe</p>
+
+              <div class="form-outline form-white mb-4">
+                <input type="text" id="typeEmailX" name="username" class="form-control form-control-lg" />
+                <label class="form-label" for="typeEmailX">Username</label>
+              </div>
+
+              <div class="form-outline form-white mb-4">
+                <input type="password" id="typePasswordX" name="pass" class="form-control form-control-lg" />
+                <label class="form-label" for="typePasswordX">Mot de passe</label>
+              </div>
+
+              <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 </form>
 </center>
 </body>

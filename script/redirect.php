a43f022e243b9b4ad31d8978f297ea02da1cc0cb
ImoriSama <80395754+ImoriSama@users.noreply.github.com>
2023-05-11 10:50:24 +0200

    Update redirect.php

--- script/redirect.php
+++ script/redirect.php
@@ -7,6 +7,19 @@ unset($files[0]);
 unset($files[1]);
 unset($files[2]);
 
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<center><h1>Voici la page Admin</h1></center>
+
+<?php
+
 foreach ($files as $value) {
     echo "<a href='../modif/".$value."'>".$value."</a><br/><br/>";
 }
